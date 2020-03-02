99a72c343d73d83a4b0cc6195f6b5d2367134d1f
Theo Johnson <txxjohnson@gmail.com>
2020-03-02 14:13:03 -0500

    Init

---
+++ scripts/Scene 01.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# GOAL: Make the player tank move left and right
+# Attach a script to Player and have it move left and round when the
+# ui_left and ui_right keys are pressed.
+#
+#
+# Create your script in the scripts folder
+
+func _ready():
+	pass # Replace with function body.
+
+	
---
+++ scripts/Scene 02.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+# GOAL: Make the turret follow the mouse
+# Attach the script you made in Scene 01. Make the turret follow the mouse
+# Attach your player script you've created before to the player in this scene.
+# Add an inherited scene for the bullet to this scene.
+# Have your player script find this bullet and use it when firing
+
+func _ready():
+	pass # Replace with function body.
+
